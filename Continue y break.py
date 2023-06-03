19438d764faf29e2d3ce73e7407cb0f5d2085e21
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-03 14:09:08 -0600

    primera version

---
+++ Continue y break.py
@@ -0,0 +1,13 @@
+#Instruccion continue y break
+
+for i in range(11):
+    if i==5:
+        continue   #Hace que continue el bucle
+    print(i)
+
+for i in range(11):
+    if i==5:
+        break    #termina con el bucle hasta la instruccion indicada
+    print(i)
+
+print("programa finalizado")
